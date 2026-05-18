bbedabd4e4c37a29797f01ab616781a22919b85f
MaiyaLi <manniemanuelsoriano@gmail.com>
2026-05-18 17:59:47 +0800

    feat: setup capacitor and android apk build workflow

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.arknights.tacticalsync',
+  appName: 'Arknights: Tactical Sync',
+  webDir: 'dist'
+};
+
+export default config;
